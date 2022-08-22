44bd7dfa5834aad4fb29b02de5b50436de851a0c
spreyo <60149407+spreyo@users.noreply.github.com>
2022-08-22 20:26:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -13,12 +13,55 @@ app.use(bodyParser.json());
 var pingHour = null;
 var pingMinute = null;
 
+
+var channelTree = {
+    "UBNT": {
+
+    },
+    "TPLINK": {
+
+    }
+}
+
+app.post('/communicate', (req, res, next)=>{
+    channelName = req.body.channel;
+    for(var i = 0; i<= Object.keys(channelTree).length; i++){
+        if(Object.keys(channelTree)[i] === req.body.channel){ // check if the requested channel exists, if yes update it
+                console.log("channel exists")
+                
+                // update the hours
+                channelTree[channelName]["hours"] = req.body.hours; 
+                
+                // update the minutes
+                channelTree[channelName]["minutes"] = req.body.minutes;
+                break;
+        }
+        else{
+            console.log("channel does not exist, creating one")
+            channelTree[channelName] = {}; // create the channel object
+            channelTree[channelName]["hours"] = req.body.hours; // add channel hours
+            channelTree[channelName]["minutes"] = req.body.minutes; // add channel minutes
+            break; // prevent creating multiple objects
+        }    
+        }
+    res.send(200);
+    })
+
+app.get("/communicate", (req, res, next)=>{
+    res.send(channelTree); // obtain channel tree object
+    res.status(200);
+})
+
+    
+
+
 app.get('/ping', (req, res, next)=>{
     res.send({hours: pingHour, minutes: pingMinute});
     console.log(pingHour, pingMinute);
     res.status(200);
     
 })
+
 
 app.post('/ping', (req, res, next)=>{
     pingHour = req.body.hours;
